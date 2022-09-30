da50e63b6f1e51b247a9c85625288c47e7348de5
JeevaRP759 <102504727+JeevaSpektra@users.noreply.github.com>
2022-09-30 14:11:38 +0530

    Add files via upload

---
+++ Collection/Inventory_insert.sql
@@ -0,0 +1,12 @@
+--Select * from CartItems
+--Select * from Carts
+
+Select * from offers
+
+--Select * from Orders
+--Select * from Payments
+Select * from paymentMethods
+Select * from ProductCategories
+Select * from products
+Select * from Reviews
+Select * from Users
